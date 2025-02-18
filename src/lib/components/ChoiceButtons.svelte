30203a1d6e3e6313d75817d3c9306cb12fa3cf70
David Sullivan <treatkor@gmail.com>
2025-02-17 16:38:48 -0800

    remove components

--- src/lib/components/ChoiceButtons.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  let { chooseGame } = $props();
-  let dis = $state(false);
-</script>
-
-<button class="againbutton" disabled={dis} onclick={() => {dis = false; chooseGame(5)}}>Regular waffle (5x5)</button>
-<button class="againbutton" disabled={dis} onclick={() => {dis = false; chooseGame(7)}}>Large waffle (7x7)</button>
