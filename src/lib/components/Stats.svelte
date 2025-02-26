edffede0ba53e3c2eef03ac7537084c280afeb4c
David Sullivan <treatkor@gmail.com>
2025-02-26 13:05:32 -0800

    remove comments

--- src/lib/components/Stats.svelte
+++ src/lib/components/Stats.svelte
@@ -11,17 +11,11 @@
   <div class="cc" data-status='i' style="width: {iwidth}%;"></div>
   <div class="cc" data-status='x'style="width: {xwidth}%;"></div>
 </div>
-<!-- <div class="stats">
-  <div class="col"><div class="minitile" data-status='c'></div><div class="val"> {flatBoard.filter(l => l.status == 'c').length}</div></div>
-  <div class="col"><div class="minitile" data-status='i'></div><div class="val"> {flatBoard.filter(l => l.status == 'i').length}</div></div>
-  <div class="col"><div class="minitile" data-status='x'></div><div class="val"> {flatBoard.filter(l => l.status == 'x').length}</div></div>
-</div> -->
 
 <style>
   .graph {
     margin: 0.75rem auto;
     display: flex;
-    /* gap: 1rem; */
     width: 100%;
     border-radius: var(--radius);
     overflow: hidden;
@@ -41,29 +35,4 @@
   [data-status='x'] {
     background-color: var(--xcolor);
   }
-
-  /* .stats {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-  }
-  .col {
-    display: flex;
-    align-items: center;
-  }
-  .col:last-child {
-    margin-left: auto;
-    
-  }
-  .col:nth-child(2) {
-    margin: 0 auto;
-  }
-
-  .val {
-    min-width: 2rem;
-  }
-
-  .minitile {
-    height: 1rem;
-    width: 1rem;
-  } */
 </style>
