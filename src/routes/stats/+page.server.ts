cc2cbf7540599d270121936b4d6ad7e6eb36a529
David Sullivan <treatkor@gmail.com>
2025-02-26 20:48:06 -0800

    adjust paths

--- src/routes/stats/+page.server.ts
+++ src/routes/stats/+page.server.ts
@@ -2,8 +2,10 @@
 // const filePath = 'static/gamesdata/games.json';
 
 export const load = async ({ fetch }) => {
+  // console.log(`process.env.URL: ${process.env.URL}`);
+  // const res = await fetch(`${process.env.URL}/.netlify/functions/read-json`);
+  const res = await fetch(`/.netlify/functions/read-json`);
 
-  const res = await fetch(`${process.env.URL}/.netlify/functions/read-json`);
   const data = await res.json();
   console.log('data: ', data);
   return { data: data };
